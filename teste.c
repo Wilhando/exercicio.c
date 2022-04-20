a6c2a336793fc21af3274359664a01fc9817ae2b
Wilhando <92072160+Wilhando@users.noreply.github.com>
2022-04-20 20:31:15 -0300

    Update teste.c

--- teste.c
+++ teste.c
@@ -4,7 +4,7 @@
 int main () {
     int A, B, X;
     scanf(" %d digite um numero: ", &A);
-    scanf(" %d digite um numero: ", &B);
+    scanf(" %d digite  outro numero: ", &B);
     X = A + B
     printf("%d", X);
     return (0);
